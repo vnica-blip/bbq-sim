0092e68cbdb5e5c351607a17f29a7dadc13c97d0
Jluz9 <jluzr9@gmail.com>
2025-10-03 19:47:53 -0400

    Programmed end cutscene transition
    Added EndCutscene script and a fade out animation

---
+++ Assets/EndCutscene.cs
@@ -0,0 +1,39 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCutscene : MonoBehaviour
+{
+    public ThirdPersonController movement;
+    //public Animator playerAnimator;
+    public GameObject levelChange;
+    public GameObject fadeOut;
+    public float transitionLength = 2f;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //other.name should equal the root of your Player object
+        if (other.tag == "Player")
+        {
+            FreezePlayer();
+            fadeOut.SetActive(true);
+            StartCoroutine(CutsceneStart());
+        }
+    }
+
+    public void FreezePlayer()
+    {
+        movement.enabled = false;
+        //playerAnimator.enabled = false;
+    }
+
+    public IEnumerator CutsceneStart()
+    {
+        yield return new WaitForSeconds(transitionLength);
+        levelChange.SetActive (true);
+        
+        yield return null;
+    }
+}
